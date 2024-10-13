d4001e7a8a2d0cf005520a93887a47a0f091ac08
naugustynowicz <nicolas.augustynowicz@gmail.com>
2024-10-13 15:36:20 +0200

    Add 1rst model and CRUD.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  get "/articles", to: "articles#index"
+  root "articles#index"
+  resources :articles
 end
