ac848e0f3bc01f465be25e05f4ec5823429c61a9
naugustynowicz <nicolas.augustynowicz@gmail.com>
2025-02-20 18:00:22 +0100

    Remove old identification system

--- app/controllers/concerns/identification.rb
+++ app/controllers/concerns/identification.rb
@@ -1,8 +1,8 @@
 module Identification
   extend ActiveSupport::Concern
 
-  included do
-    skip_before_action :verify_authenticity_token
-    http_basic_authenticate_with name: "dhh", password: "secret", except: [ :index, :show, :create, :update, :destroy ]
-  end
+  # included do
+  #   skip_before_action :verify_authenticity_token
+  #   http_basic_authenticate_with name: "dhh", password: "secret", except: [ :index, :show, :create, :update, :destroy ]
+  # end
 end
