cd07cc025e21b6d86601b835bee4ea5684decb88
naugustynowicz <nicolas.augustynowicz@gmail.com>
2025-03-21 16:42:40 +0100

    auth en pls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include ActionController::Cookies
-  # after_action :set_csrf_cookie
+  after_action :set_csrf_cookie
+  # after_action :set_csrf_token
   # def fallback_index_html
   #   render file: "public/index.html"
   # end
@@ -14,7 +15,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception # , prepend: true
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # skip_before_filter :verify_authenticity_token, only: :create
+  # skip_before_action :verify_authenticity_token, only: :create
 
   def index
     # just do nothing
@@ -28,12 +29,20 @@ class ApplicationController < ActionController::Base
   end
 
   # WIP
-  # def set_csrf_cookie
-  #   cookies["CSRF-TOKEN"] = {
-  #     value: form_authenticity_token,
-  #     secure: true,
-  #     same_site: :strict
-  #     # domain: "localhost:3000/ui/"
-  #   }
+  def set_csrf_cookie
+    cookies["CSRF-TOKEN"] = {
+      value: form_authenticity_token,
+      secure: true,
+      same_site: :strict
+      # domain: "localhost:3000/ui/"
+    }
+  end
+
+  # def set_csrf_token
+  #   cookies["XSRF-TOKEN"] = form_authenticity_token if protect_against_forgery?
+  # end
+  #
+  # def verified_request?
+  #   super || valid_authenticity_token?(session, request.headers["X-XSRF-TOKEN"])
   # end
 end
