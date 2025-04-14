2b5bdb846136962455e7a593e51951f534e487f1
naugustynowicz <nicolas.augustynowicz@gmail.com>
2025-04-14 17:40:24 +0200

    clean up

--- app/models/event.rb
+++ app/models/event.rb
@@ -13,6 +13,17 @@ class Event < ApplicationRecord
     self.users.push user
   end
 
+  def generate_tree_bracket
+    first_round_match_list = generate_bracket
+    final_match = Match.create!(event: self)
+    @current_round_matches = []
+    generate_tree(first_round_match_list, final_match)
+    update! bracket: final_match
+    final_match
+  end
+
+  private
+
   def generate_bracket
     shuffled_users = users.shuffle
 
@@ -26,22 +37,11 @@ class Event < ApplicationRecord
     first_round_match_list
   end
 
-  def generate_tree_bracket
-    first_round_match_list = generate_bracket
-    final_match = Match.create!(event: self)
-    @current_round_matches = []
-    generate_tree(first_round_match_list, final_match)
-    update! bracket: final_match
-    final_match
-  end
-
   def generate_tree(array, node)
     return if array.length < 2
     return unless array.length % 2 == 0
 
     if array.length.eql? 2
-      # first_round_match_1 = Tree::TreeNode.new("name1", array.first)
-      # first_round_match_2 = Tree::TreeNode.new("name2", array.last)
       @current_round_matches.push array.first
       @current_round_matches.push array.second
       node.update! previous_match_1: array.first.id, previous_match_2: array.second.id
